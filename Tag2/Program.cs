b6239da1a24e47a5e40fa644f27bd74f9d5409dd
emir067 <162762377+emir067@users.noreply.github.com>
2026-01-29 01:35:42 +0100

    Projektdateien hinzufügen.

---
+++ Tag2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class LogikRechner
+{
+    static void Main()
+    {
+        Console.WriteLine("Mini Logik Rechner");
+        Console.WriteLine("Bitte gib eine zahl ein");
+        if (int.TryParse(Console.ReadLine(), out int zahl))
+        {
+            if (zahl < 0)
+                Console.WriteLine("Negative Zahl");
+            else if (zahl > 0)
+                Console.WriteLine("Positive Zahl");
+            else
+                Console.WriteLine("Die Zahl ist 0");
+        }
+        else
+        {
+            Console.WriteLine("Keine gültige Zahl!");
+        }
+    }
+}
